ed66305ff366917eeb6ecef7fc516f5e3b957f1e
WHITERBOSS <orlandocumandala329@gmail.com>
2026-06-08 19:26:17 +0100

    Update Mensagem.java
    construtor mensagem

--- src/rede/social/Mensagem.java
+++ src/rede/social/Mensagem.java
@@ -47,5 +47,11 @@ public class Mensagem {
         this.dataEnvio = dataEnvio;
     }
     
-    
+    public void mensagem (  String conteudo,int remetenteID, int destinatarioID, int LocalDateTime dataEnvio){
+this.conteudo=conteudo;
+        this. destinatarioID= destinatarioID;
+        this.dataEnvio=dataEnvio;
+        this.remetenteID=remetenteID;
+
+    }
 }
